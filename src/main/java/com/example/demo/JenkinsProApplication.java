f60a60d712e5cb1413fb535bbc61be694617d98e
anoop abbot <anoop.abbot007@gmail.com>
2020-04-22 05:57:52 -0700

    changes done and committed

---
+++ src/main/java/com/example/demo/JenkinsProApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProApplication {
+	public static Logger LOGGER = LoggerFactory.getLogger(JenkinsProApplication.class);
+
+	public static void main(String[] args) {
+		
+		LOGGER.info("Application is going to start");
+		
+		SpringApplication.run(JenkinsProApplication.class, args);
+		LOGGER.info("Application Ended here");
+	}
+
+}
